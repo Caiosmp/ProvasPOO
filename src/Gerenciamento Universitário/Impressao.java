038c52c9dc927d3c775705bb9368397a0523a359
Caio <caiosampaio044@gmail.com>
2023-11-03 00:40:25 -0300

    Prova Gerenciamento

--- src/Gerenciamento Universitário/Impressao.java
+++ src/Gerenciamento Universitário/Impressao.java
@@ -1,4 +1,3 @@
-
 public class Impressao {
     
     public void laboratorios() {
--- src/Gerenciamento Universitário/Main.java
+++ src/Gerenciamento Universitário/Main.java
@@ -6,6 +6,5 @@ public class Main {
        impressao.laboratorios();
        impressao.conjProfessor();
        impressao.conjAlunos();
-
     }
 }
